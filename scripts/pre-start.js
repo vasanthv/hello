72cc9cc64366b33cf349c04fe489bce4e99d4ad7
vasanth <imvasanthv@gmail.com>
2024-12-26 07:16:44 -0500

    Removed analytics

--- scripts/pre-start.js
+++ scripts/pre-start.js
@@ -11,23 +11,4 @@ if (process.env.NODE_ENV === "production") {
 	fs.writeFileSync(path.join(__dirname, "../www/sw.js"), newServiceWorkerContents);
 
 	console.log("Service worker file updated to version", VERSION);
-
-	// if (process.env.ANALYTICS_SCRIPT) {
-	// 	console.log("Inserting Analytics script tag");
-
-	// 	const scriptTag = `<script async defer src="${process.env.ANALYTICS_SCRIPT}"></script>`;
-
-	// 	const filesToBeTracked = [
-	// 		"../www/index.html",
-	// 		"../static/bookmarklet.html",
-	// 		"../static/privacy.html",
-	// 		"../static/terms.html",
-	// 	];
-
-	// 	filesToBeTracked.forEach((file) => {
-	// 		const fileContents = fs.readFileSync(path.join(__dirname, file)).toString();
-	// 		const newFileContents = fileContents.replace("<!-- INSERT_ANALYTICS_SCRIPT -->", scriptTag);
-	// 		fs.writeFileSync(path.join(__dirname, file), newFileContents);
-	// 	});
-	// }
 }
