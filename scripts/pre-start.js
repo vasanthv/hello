f78476fd3e9a33be95e4939f3d2f14015af4f5f9
vasanth <imvasanthv@gmail.com>
2025-08-02 14:55:08 -0400

    Fixed line breaks

--- scripts/pre-start.js
+++ scripts/pre-start.js
@@ -17,8 +17,8 @@ const replaceServiceWorkerVersion = () => {
 const addTurnServers = () => {
 	const iceFileContents = fs.readFileSync(path.join(__dirname, "../public/ice-config.js")).toString();
 
-	const udp = `{ urls: "${process.env.TURN_URL_UDP}", username: "${process.env.TURN_USERNAME}", credential: "${process.env.TURN_PASSWORD}" }`;
-	const tcp = `{ urls: "${process.env.TURN_URL_TCP}", username: "${process.env.TURN_USERNAME}", credential: "${process.env.TURN_PASSWORD}" }`;
+	const udp = `{ urls: "${process.env.TURN_URL_UDP}", username: "${process.env.TURN_USERNAME}", credential: "${process.env.TURN_PASSWORD}" },\n`;
+	const tcp = `{ urls: "${process.env.TURN_URL_TCP}", username: "${process.env.TURN_USERNAME}", credential: "${process.env.TURN_PASSWORD}" },\n`;
 
 	let turnServers = "";
 
@@ -33,7 +33,7 @@ const addTurnServers = () => {
 
 	fs.writeFileSync(path.join(__dirname, "../public/ice-config.js"), newIceFileContents);
 
-	console.log("Service worker file updated to version", turnServers);
+	console.log("TURN servers added:", turnServers);
 };
 
 if (process.env.NODE_ENV === "production") {
